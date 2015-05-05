cfeb41d3d95911a91ba59909a4ac8aded49afebf
Alex Chojnacki <thealex@umich.edu>
2015-05-05 16:34:46 -0400

    changes!

---
+++ javascript/http_client.js
@@ -0,0 +1,15 @@
+// Takes a URL as an argument and prints the data in the HTTP response object.
+var datURL = process.argv[2];
+// require the 'http' library
+var http = require('http');
+
+http.get(datURL, function (response) {
+    response.setEncoding('utf8');
+    // When the data becomes available on the response object, do this ...
+    response.on("data", function (data) {
+        data.forEach(function (datum) {
+            console.log(datum);
+        });
+    });
+});
+
