5b1aa0db0127db25339efd83ad2160ba3e361deb
Alex Chojnacki <theProfessional@Alexs-MacBook-Pro.local>
2015-05-06 01:35:44 -0400

    fine, jshint. I added your damn semicolons- even though there's barely a need for server side minimization.

--- javascript/mod/filter_mod.js
+++ javascript/mod/filter_mod.js
@@ -1,10 +1,10 @@
 // We use this library to access the file system.
 //  fs => readdir(dir, <callback>)
-var fs = require('fs')
+var fs = require('fs');
 
 // We use the path library for it's string manipulations
 //  path => extname(<fileName>) :: returns the extension on the file.
-var path = require('path')
+var path = require('path');
 
 // This export allows other files to access this function.
 //  Note :: because this function is asynchronous, we include a callback function parameter.
@@ -15,7 +15,7 @@ module.exports = function (dir, extString, callback) {
         if (err) {
             // ... specifically, with the 'err' parameter on the callback that was passed in.
             //      We're passing it "up" the callback chain.
-            return callback(err)
+            return callback(err);
         }
 
         // Look at each element in the list of file names in the specified directory ...
@@ -24,13 +24,13 @@ module.exports = function (dir, extString, callback) {
         data = data.filter(function (file) {
             // ... and filter the list base on this function we pass in.
             // Membership in 'data' is now determined by this filter function returning an element.
-            return path.extname(file) === '.' + extString
-        })
+            return path.extname(file) === '.' + extString;
+        });
 
         // By this point, we can now return the data into the callback we passed into this function.
         //  SideNote :: Notice the idiomatic parameters. We're setting err == null and the data section
         //  to the motified data value we created in this function.
-        callback(null, data)
-    })
-}
+        callback(null, data);
+    });
+};
 
